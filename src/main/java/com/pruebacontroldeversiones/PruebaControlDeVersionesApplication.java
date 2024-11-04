f02ddc02d69095988e2622985f61f33b5b38e152
carloshernandez176264 <carloshernandez176264@hotmail.com>
2024-11-04 11:00:25 -0500

    Initial commit

---
+++ src/main/java/com/pruebacontroldeversiones/PruebaControlDeVersionesApplication.java
@@ -0,0 +1,13 @@
+package com.pruebacontroldeversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaControlDeVersionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaControlDeVersionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pruebacontroldeversiones/PruebaControlDeVersionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebacontroldeversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaControlDeVersionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
